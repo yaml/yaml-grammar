24c97399a463f7c64cf3fbbce298e0ed109fed3f
Ingy döt Net <ingy@ingy.net>
2020-09-05 09:54:16 -0700

    Switch perl testing to TestML

--- parser/src/common.mk
+++ parser/src/common.mk
@@ -5,6 +5,7 @@ GENERATOR := $(ROOT)/parser/bin/generate-yaml-grammar
 GENERATOR_LIB := $(ROOT)/parser/lib/generate-yaml-grammar-$(LANG).coffee
 YAML_SPEC_YAML := $(ROOT)/spec-1.2.yaml
 
+tests := test/*.tml
 
 .DELETE_ON_ERROR:
 
@@ -12,12 +13,12 @@ default:
 
 build: $(GRAMMAR)
 
-test: build
-	TRACE=$(TRACE) $(BIN) ./test/test.$(EXT)
+test: build ../testml
+	TRACE=$(trace) DEBUG=$(debug) prove -v $(tests)
 
 .PHONY: trace
 trace: build
-	-TRACE=1 DEBUG=$(DEBUG) \
+	-TRACE=1 DEBUG=$(debug) \
 	    $(BIN) test/test.$(EXT) \
 	    '[1,2 2  ,333,]' \
 	    |& grep -v '^Parse' \
@@ -29,3 +30,6 @@ $(GRAMMAR): $(YAML_SPEC_YAML) $(GENERATOR_LIB)
 	    --to=$(LANG) \
 	    --rule=l-yaml-stream \
 	> $@
+
+../testml:
+	git clone https://github.com/testml-lang/testml $@
---
+++ parser/src/perl/test/TestMLBridge.pm
@@ -0,0 +1,22 @@
+use v5.12;
+package TestMLBridge;
+use base 'TestML::Bridge';
+
+use lib 'lib';
+
+use Prelude;
+use Parser;
+use TestReceiver;
+
+sub parse {
+  my ($self, $yaml) = @_;
+
+  my $parser = Parser->new(TestReceiver->new);
+
+  eval { $parser->parse($yaml) };
+  return $@
+    ? $@
+    : $parser->{receiver}->output;
+}
+
+1;
